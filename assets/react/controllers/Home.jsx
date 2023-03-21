bf0d1def0564d7d3bf8186a87d04248f3840e50e
Cindy <telcindy59@gmail.com>
2023-03-21 17:25:45 +0100

    commit

--- assets/react/controllers/Home.jsx
+++ assets/react/controllers/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home({companies}) {
+    let [search, setSearch] = useState();
     return (
         <div>
             <h2  className="text-yellow-700 font-bold text-2xl text-center mt-5">Hello React</h2>
+           <input value={search} onChange={(e) =>setSearch(e.target.value)} />
             <ul className="mx-5">
-                {companies.map(company=><li>{company}</li>)}
+                {companies.filter(company =>company.includes(search)).map(company=><li>{company}</li>)}
             </ul>
         </div>
     )
