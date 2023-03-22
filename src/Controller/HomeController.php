4ed5a66be646e60cdef8b1e717b79d92155c46f4
Cindy <telcindy59@gmail.com>
2023-03-22 17:29:23 +0100

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ReviewRepository $repository): Response
     {
         return $this->render('home/index.html.twig', [
             'companies' => ['Apple', 'Boxydev', 'Microsoft'],
+            'reviews' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/review/new', name: 'app_review_new')]
+    #[IsGranted('ROLE_USER')]
+    public function new() 
+    {
+        $review = new Review();
+        $form= $this->createForm(ReviewType::class, $review);
+        return $this->render('review/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
