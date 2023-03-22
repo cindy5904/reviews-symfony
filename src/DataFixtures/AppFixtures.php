e674e6346a3382e68300326cfb9e8e08c1394a49
Cindy <telcindy59@gmail.com>
2023-03-22 10:04:20 +0100

    preparer le user entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setName('Cindy');
+        $user->setEmail('cindy@gmail.com');
+        $user->setPassword($this->hasher->hashPassword($user, 'password'));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->flush();
+    }
+}
